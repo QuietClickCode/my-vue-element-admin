38bb5385b498f69e736225fab997b75155d558f4
zjjlovetj <2572417548@qq.com>
2019-12-22 00:01:09 +0800

    登录接口测通,后台应该用户名和密码,除了,其他参数可选

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,12 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['plugin:vue/recommended', 'eslint:recommended'],
+  /*extends: ['plugin:vue/recommended', 'eslint:recommended'],*/
 
   // add your custom rules here
   //it is base on https://github.com/vuejs/eslint-config-vue
   rules: {
-    "vue/max-attributes-per-line": [2, {
+   /* "vue/max-attributes-per-line": [2, {
       "singleline": 10,
       "multiline": {
         "max": 1,
@@ -193,6 +193,6 @@ module.exports = {
     'object-curly-spacing': [2, 'always', {
       objectsInObjects: false
     }],
-    'array-bracket-spacing': [2, 'never']
+    'array-bracket-spacing': [2, 'never']*/
   }
 }
