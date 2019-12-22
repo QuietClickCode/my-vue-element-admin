806638693c760fa6e431a0a356445d5d1cf33a83
zjjlovetj <2572417548@qq.com>
2019-12-22 21:05:50 +0800

    实现退出

--- src/api/user.js
+++ src/api/user.js
@@ -18,7 +18,8 @@ export function getInfo(token) {
 
 export function logout() {
   return request({
-    url: '/user/logout',
+    /*url: '/user/logout',*/
+    url: '/logout',
     method: 'post'
   })
 }
