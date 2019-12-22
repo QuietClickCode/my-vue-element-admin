d1640c8d126626edff865725ffbe9addc347be1f
zjjlovetj <2572417548@qq.com>
2019-12-22 21:49:22 +0800

    系统日志迁移到vue-element-admin了,想办法解决有时候页面router无限嵌套卡死的问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -19,7 +19,8 @@ service.interceptors.request.use(
             // let each request carry token
             // ['X-Token'] is a custom headers key
             // please modify it according to the actual situation
-            config.headers['X-Token'] = getToken()
+            /*config.headers['X-Token'] = getToken()*/
+            config.headers['token'] = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ6amoifQ.WduZcdFyZSMdrx82JOYFbAKoDPQ4evDFPvt6i6USK5Y'
         }
         return config
     },
@@ -73,12 +74,13 @@ service.interceptors.response.use(
         if (res.status == 0) {
             return res
         } else {
-            Message({
+          /*  Message({
                 message: res.msg || 'Error',
                 type: 'error',
                 duration: 5 * 1000
             })
-            return Promise.reject(new Error(res.message || 'Error'))
+            return Promise.reject(new Error(res.message || 'Error'))*/
+            return res
         }
     },
     error => {
