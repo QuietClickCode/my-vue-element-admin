fc784169fd8e395ea8607289f82a8530838b9580
QuietClickCode <2572417548@qq.com>
2020-02-09 21:54:17 +0800

    修改文件上传地址

--- src/views/fileupload/index.vue
+++ src/views/fileupload/index.vue
@@ -11,7 +11,7 @@
             :headers="header"
             :on-exceed="beyondNumber"
             :on-success="handleAvatarSuccess"
-            action="http://localhost/uploadImg"
+            action="https://114.55.94.186/uploadImg"
             :file-list="fileList"
             multiple
             :on-remove="handleRemove">
